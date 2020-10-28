eb2d744a6cc4258f1758022456a527fb00844a45
vinicius Fernandes <vinicim.fernandez1@gmail.com>
2020-10-28 09:44:58 -0300

    BeTheHero back-end finalizado

---
+++ tests/integration/ong.spec.js
@@ -0,0 +1,27 @@
+const request = require('supertest');
+const app = require('../../src/app');
+const connection = require('../../src/database/connection');
+
+describe('ONG', ()=>{
+    beforeEach( async () => {
+        await connection.migrate.rollback();
+        await connection.migrate.latest();
+    });
+
+    afterAll( async () => {      
+        await connection.destroy();
+    });
+
+    it('should be able to create a new ONG', async () =>{
+        const response = await request(app).post('/ongs').send({
+            name : "APAD7",
+	        email : "contato@teste.com",
+	        whatsapp: "1111111111",
+	        city : "Uberba",
+		    uf : "MG"
+        });
+
+        expect(response.body).toHaveProperty('id');
+        expect(response.body.id).toHaveLength(8);
+    });
+});
